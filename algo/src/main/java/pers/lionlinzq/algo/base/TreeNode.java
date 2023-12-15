faf574e9775ed11a5c555b09d54b5da84740c969
lionlinzq <niodindeco@hotmail.com>
2023-12-15 16:20:04 +0800

    init: 初始化项目

---
+++ algo/src/main/java/pers/lionlinzq/algo/base/TreeNode.java
@@ -0,0 +1,26 @@
+package pers.lionlinzq.algo.base;
+
+/**
+ * 树节点
+ *
+ * @author lin
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
