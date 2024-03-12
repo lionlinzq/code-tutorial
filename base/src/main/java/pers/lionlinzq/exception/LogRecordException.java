aee190f5360427166663a51c347a366f6bf9bddb
lzq <niodindeco@hotmail.com>
2024-03-12 15:28:57 +0800

    feat:  增加文件

--- base/src/main/java/pers/lionlinzq/exception/LogRecordException.java
+++
@@ -1,8 +0,0 @@
-package pers.lionlinzq.exception;
-
-public class LogRecordException extends RuntimeException {
-
-    public LogRecordException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
---
+++ utils/src/main/java/pers/lionlinzq/Main.java
@@ -0,0 +1,11 @@
+package pers.lionlinzq;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] ints = new int[0];
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+}
