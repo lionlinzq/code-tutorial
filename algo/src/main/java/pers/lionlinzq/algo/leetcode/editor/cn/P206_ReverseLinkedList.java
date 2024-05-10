279f062f92c4fb20a213a171f13ddae45083ab7f
lionlinzq <niodindeco@hotmail.com>
2024-05-10 09:58:41 +0800

    feat: 增加每日一题

---
+++ algo/src/main/java/pers/lionlinzq/algo/leetcode/editor/cn/P206_ReverseLinkedList.java
@@ -0,0 +1,115 @@
+package pers.lionlinzq.algo.leetcode.editor.cn;
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * <p>
+ * Related Topics 递归 链表 👍 3501 👎 0
+ */
+
+import pers.lionlinzq.algo.base.ListNode;
+
+import java.time.Period;
+
+/**
+ * 反转链表
+ *
+ * @author lzq
+ */
+public class P206_ReverseLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new P206_ReverseLinkedList().new Solution();
+
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            while (head != null) {
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+        }
+
+
+        /**
+         * 反向列表,递归解法
+         *
+         * @param head 头
+         * @return {@link ListNode}
+         */
+        public ListNode reverseList2(ListNode head) {
+            ListNode prev = null;
+            while (head != null) {
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+        }
+
+        private ListNode dfs(ListNode cur){
+            if (cur == null){
+                return null;
+            }
+            return dfs(cur.next);
+        }
+
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
