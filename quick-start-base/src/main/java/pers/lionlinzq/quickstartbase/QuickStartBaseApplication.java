e910877a62f0022aa0683ccbe9fc42ca9e3331d8
lionlinzq <niodindeco@hotmail.com>
2024-06-16 23:31:17 +0800

    feat: 提交一下

---
+++ quick-start-base/src/main/java/pers/lionlinzq/quickstartbase/QuickStartBaseApplication.java
@@ -0,0 +1,13 @@
+package pers.lionlinzq.quickstartbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStartBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartBaseApplication.class, args);
+    }
+
+}
---
+++ quick-start-base/src/test/java/pers/lionlinzq/quickstartbase/QuickStartBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.lionlinzq.quickstartbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
